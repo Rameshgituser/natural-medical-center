911b1d4f724e94dbc6048431bad0d0ff6caf4db4
Rameshgituse <rameshraj3298@gmail.com>
2024-02-05 07:05:21 +0530

    font alignment fixed

--- assets/js/medical.js
+++ assets/js/medical.js
@@ -1,76 +1,63 @@
-const navToggle=document.querySelector('.nav-toggler');
-  const navlinks=document.querySelectorAll('ul.menus>li>a');
-  
-navToggle.addEventListener('click',changemenustate);
+const navToggle = document.querySelector(".nav-toggle");
+const navlinks = document.querySelectorAll("ul.menus>li>a");
 
-    function changemenustate(){
-      const headermenu=document.querySelector('ul.menus')
+navToggle.addEventListener("click", changemenustate);
 
-      const navicon=document.querySelectorAll('.nav-icon');
-      /*show menu */
-    headermenu.classList.toggle("show");
-    // headermenu.remove.toggle('ul.header--menu')
-    navicon.forEach(icon=>{
-       icon.classList.toggle("hidden");
-    })
-      
+function changemenustate() {
+  const headermenu = document.querySelector("ul.menus");
+
+  const navicon = document.querySelectorAll(".nav-icon");
+
+  /*show menu */
+  headermenu.classList.toggle("show");
+  // headermenu.remove.toggle('ul.header--menu')
+  navicon.forEach((icon) => {
+    icon.classList.toggle("hidden");
+  });
+}
+
+function fixednavbar() {
+  const headerdom = document.querySelector(".navbar-fixed");
+  const navheight = headerdom.clientHeight;
+  //  console.log(navheight);
+
+  window.addEventListener("scroll", () => {
+    let scroll = window.pageYOffset || document.documentElement.scrollTop;
+    //  console.log(scroll);
+
+    if (scroll >= navheight) {
+      headerdom.classList.add(".navbar-fixed");
+    } else {
+      headerdom.classList.remove(".navbar-fixed");
     }
+  });
+}
 
+function onmenuclick() {
+  //   console.log(navlinks);
 
-    
+  for (let i = 0; i < navlinks.length; i++) {
+    navlinks[i].addEventListener("click", changemenustate);
+  }
+}
 
-    function fixednavbar(){
-         
-        const headerdom= document.querySelector('.navbar-fixed');
-          const navheight=headerdom.clientHeight;
-         //  console.log(navheight);
-               
-         window.addEventListener('scroll',()=>{
+window.addEventListener("scroll", reveal);
 
-            let scroll=window.pageYOffset || document.documentElement.scrollTop;
-            //  console.log(scroll);
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
 
-             if(scroll>=navheight){
-              headerdom.classList.add('.navbar-fixed')
-             
-          
-             }
-             else{
-              headerdom.classList.remove('.navbar-fixed');
-             }
-          })
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].remove.classList.remove("active");
     }
+  }
+}
 
-
-    function onmenuclick(){
-      //   console.log(navlinks);
-
-        for(let i=0;i<navlinks.length;i++){
-           navlinks[i].addEventListener('click',changemenustate)
-        }
-    }
-
-
-      window.addEventListener('scroll',reveal);
-
-       function reveal(){
-          var reveals=document.querySelectorAll('.reveal');
-
-          for(var i=0;i<reveals.length;i++){
-              var windowheight=window.innerHeight;
-                var revealtop=reveals[i].getBoundingClientRect().top;
-                var revealpoint=150;
-
-                if(revealtop < windowheight - revealpoint){
-                  reveals[i].classList.add('active');
-                }
-                else{
-                  reveals[i].remove.classList.remove('active');
-                }
-          }
-       }
-
-
-    fixednavbar()
-    onmenuclick()
-  
+fixednavbar();
+onmenuclick();
