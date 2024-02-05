f0d65e48b03721b934d4367a412fc5acd6385ae7
Rameshgituse <rameshraj3298@gmail.com>
2024-02-05 08:09:59 +0530

    navbar styling

--- assets/js/medical.js
+++ assets/js/medical.js
@@ -1,63 +1,26 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navlinks = document.querySelectorAll("ul.menus>li>a");
-
-navToggle.addEventListener("click", changemenustate);
-
-function changemenustate() {
-  const headermenu = document.querySelector("ul.menus");
-
-  const navicon = document.querySelectorAll(".nav-icon");
-
-  /*show menu */
-  headermenu.classList.toggle("show");
-  // headermenu.remove.toggle('ul.header--menu')
-  navicon.forEach((icon) => {
-    icon.classList.toggle("hidden");
-  });
-}
-
-function fixednavbar() {
-  const headerdom = document.querySelector(".navbar-fixed");
-  const navheight = headerdom.clientHeight;
-  //  console.log(navheight);
+function setMenuActive() {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("ul.menus>a");
 
   window.addEventListener("scroll", () => {
-    let scroll = window.pageYOffset || document.documentElement.scrollTop;
-    //  console.log(scroll);
+    let current = "";
 
-    if (scroll >= navheight) {
-      headerdom.classList.add(".navbar-fixed");
-    } else {
-      headerdom.classList.remove(".navbar-fixed");
-    }
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach((li) => {
+      li.classList.remove("active");
+      if (current == li.getAttribute("href").split("#")[1]) {
+        li.classList.add("active");
+      }
+    });
   });
 }
 
-function onmenuclick() {
-  //   console.log(navlinks);
-
-  for (let i = 0; i < navlinks.length; i++) {
-    navlinks[i].addEventListener("click", changemenustate);
-  }
-}
-
-window.addEventListener("scroll", reveal);
-
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
-
-    if (revealtop < windowheight - revealpoint) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].remove.classList.remove("active");
-    }
-  }
-}
-
-fixednavbar();
-onmenuclick();
+// setMenuActive();
