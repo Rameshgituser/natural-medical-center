917748569e47494bffe04c0b9763078a41a89fef
Rameshgituse <rameshraj3298@gmail.com>
2024-02-17 05:50:37 +0530

    nav menu fixed

--- assets/js/medical.js
+++ assets/js/medical.js
@@ -24,3 +24,53 @@ function setMenuActive() {
 }
 
 // setMenuActive();
+
+// show/hide for faq answer
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    //change icon
+
+    const icon = faq.querySelector(".faqs__icon i");
+    console.log(icon);
+
+    if (icon.className === "fa-solid fa-plus") {
+      icon.className = "fa-solid fa-minus";
+    } else {
+      icon.className = "fa-solid fa-plus";
+    }
+  });
+});
+
+//show /hide nav menu
+
+const menu = document.querySelector(".nav__menu");
+const menubtn = document.querySelector("#open-menu-btn");
+const closebtn = document.querySelector("#close-menu-btn");
+
+menubtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closebtn.style.display = "inline-block";
+  menubtn.style.display = "none";
+});
+
+//close nav menu
+const closenav = () => {
+  menu.style.display = "none";
+  closebtn.style.display = "none";
+  menubtn.style.display = "inline-block";
+};
+
+closebtn.addEventListener("click", closenav);
+
+//change navbar styles on scroll
+
+window.addEventListener("scroll", () => {
+  document
+    .getElementById("home")
+    .classList.toggle("window-scroll", window.scrollY > 100);
+});
